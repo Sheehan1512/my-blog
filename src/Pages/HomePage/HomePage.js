164f5772048ddea9f3c2e02b07ea8a5c298086c0
Sheehan1512 <sheehanvy@gmail.com>
2022-02-04 10:22:36 +0600

    completed locally

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -7,7 +7,7 @@ import Intro from "./Intro/Intro";
 function HomePage() {
     return(
         <section id="homepage">
-            <img src={SheehanDP} id="sheehan-dp" />
+            <img src={SheehanDP} id="sheehan-dp" alt="Sheehan er Chobi" />
             <Intro />
         </section>
     );
